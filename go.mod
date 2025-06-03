0c2076b65c3336915e8aab8c17f850fcf3e70b20
edickens09 <edickens09@gmail.com>
2025-06-02 22:02:38 -0400

    playout achieved

--- go.mod
+++ go.mod
@@ -2,9 +2,12 @@ module player
 
 go 1.24.0
 
-require (
-	github.com/gopxl/beep/v2 v2.1.1
-	github.com/pion/opus v0.0.0-20250423145807-4aaa26789cff
-)
+require github.com/gopxl/beep/v2 v2.1.1
 
-require github.com/pkg/errors v0.9.1 // indirect
+require (
+	github.com/ebitengine/oto/v3 v3.3.2 // indirect
+	github.com/ebitengine/purego v0.8.0 // indirect
+	github.com/hajimehoshi/go-mp3 v0.3.4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,21 +1,37 @@
 package main
 
 import (
-	"bytes"
-	"time"
+	"log"
 	"os"
+	"time"
 
-	"github.com/pion/opus"
-	"github.com/pion/opus/oggreader"
 	"github.com/gopxl/beep/v2"
+	"github.com/gopxl/beep/v2/mp3"
 	"github.com/gopxl/beep/v2/speaker"
-
 )
 
-func main () {
-	f, err := os.Open("./sample4.opus")
+func main() {
 
+	// open mp3 file and report error if it happens
+	fileName := os.Args[1]
+	f, err := os.Open(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer streamer.Close()
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+
+	<-done
 }
